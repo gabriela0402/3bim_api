e7c79db98f903dd94aa17ba5dc0bad3dc2f105e5
gabriela0402 <gabiromano2018@gmail.com>
2026-08-26 17:56:58 -0300

    Aula03 - Testes unitários

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ from schemas import ProdutoCreate, ProdutoResponse, PetBase, PetResponse, PetCre
 from fastapi.middleware.cors import CORSMiddleware
 
 
-Base.metadata.create_all(bind=engine) # cria as tabelas, se ainda não existirem
+# Base.metadata.create_all(bind=engine) # cria as tabelas, se ainda não existirem
 app = FastAPI()
+
+@app.on_event("startup")
+def criar_tabelas():
+    Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,6 +22,9 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+def buscar_produto(db: Session, produto_id: int):
+    return db.query(ProdutoDB).filter(ProdutoDB.id == produto_id).first()
 
 @app.get('/produtos', response_model=list[ProdutoResponse])
 def listar_produtos(db: Session = Depends(get_db)):
@@ -33,14 +40,14 @@ def criar_produto(produto: ProdutoCreate, db: Session = Depends(get_db)):
 
 @app.get('/produtos/{produto_id}', response_model=ProdutoResponse)
 def obter_produto(produto_id: int, db: Session = Depends(get_db)):
-    produto = db.query(ProdutoDB).filter(ProdutoDB.id == produto_id).first()
+    produto = buscar_produto(db, produto_id)
     if produto is None:
         raise HTTPException(status_code=404, detail='Produto não encontrado')
     return produto
 
 @app.delete('/produtos/{produto_id}', status_code=204)
 def remover_produto(produto_id: int, db: Session = Depends(get_db)):
-    produto = db.query(ProdutoDB).filter(ProdutoDB.id == produto_id).first()
+    produto = buscar_produto(db, produto_id)
     if produto is None:
         raise HTTPException(status_code=404, detail='Produto não encontrado')
     db.delete(produto)
@@ -49,7 +56,7 @@ def remover_produto(produto_id: int, db: Session = Depends(get_db)):
 @app.put('/produtos/{produto_id}', response_model=ProdutoResponse)
 def atualizar_produto(produto_id: int, dados: ProdutoCreate, db:
 Session = Depends(get_db)):
-    produto = db.query(ProdutoDB).filter(ProdutoDB.id == produto_id).first()
+    produto = buscar_produto(db, produto_id)
     if produto is None:
         raise HTTPException(status_code=404, detail='Produto não encontrado')
     produto.nome = dados.nome
